b90fea288abd2b6ab0282af67ecdac08cf6aad90
ChangAn <1533167307@qq.com>
2022-08-08 18:51:11 +0800

    0-0

--- 第八章编程题/第八章编程题.c
+++ 第八章编程题/第八章编程题.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 void one(void);//1
+void two(void);//two
 int main(void)
 {
-	one();
+	//one();
+	two();
 	return 0;
 }
 
@@ -12,4 +14,27 @@ void one(void)
 	while (getchar() != EOF)
 		i++;
 	printf("%d", i);
+}
+
+void two(void)
+{
+	int i = 0;
+	char ch;
+	while ((ch=getchar()) != EOF)
+	{
+		if (i == 9)
+		{
+			putchar('\n');
+			i = 0;
+		}
+		if (ch > ' ')
+			printf(" %c--%3d ", ch, ch);
+		else if(ch=='\n')
+			printf(" \\n--%3d ", ch);
+		else if (ch == '\t')
+			printf(" \\t--%3d ", ch);
+		else 
+			printf("^%c--%3d ", ch+64, ch);
+		i++;
+	}
 }
