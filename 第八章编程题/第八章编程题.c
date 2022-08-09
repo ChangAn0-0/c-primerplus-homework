80550a81ee0aea1b35aa8c3958cbc427db4b3397
ChangAn <1533167307@qq.com>
2022-08-09 16:06:46 +0800

    0-0

--- 第八章编程题/第八章编程题.c
+++ 第八章编程题/第八章编程题.c
@@ -1,10 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 void one(void);//1
 void two(void);//two
+void eight(void);//8
+float get_num(int n);
 int main(void)
 {
 	//one();
-	two();
+	//two();
+	eight();
 	return 0;
 }
 
@@ -37,4 +41,47 @@ void two(void)
 			printf("^%c--%3d ", ch+64, ch);
 		i++;
 	}
+}
+
+void eight(void)
+{
+	char ch;
+	int a = 1, b = 2;
+	//float num1, num2;
+	double num;
+
+start:printf("请选择计算方式\n");
+	printf("a.add  s.subtract  m.multiply  d.divide\n");
+	printf("q.quit\n");
+
+	switch (ch = getchar())
+	{		
+	case'a': {
+		num = (double)get_num(a) + get_num(b);
+		printf("%f", num); goto start;
+	}
+	case's': {
+		num = (double)get_num(a) - get_num(b);
+		printf("%f", num); goto start;
+	}
+	case'm': {
+		num = (double)get_num(a) * get_num(b);
+		printf("%f", num); goto start;
+	}
+	case'd': {
+		num = get_num(a) / get_num(b);
+		printf("%f", num); goto start;
+	}
+	case'q':break;
+	default:printf("未知指令"); goto start;
+	}
+}
+
+float get_num(int n)
+{
+	float num;
+get:printf("请输入值%d", n);
+	if (scanf("%f", &num)==0)
+		goto get;
+	return num;
 }
