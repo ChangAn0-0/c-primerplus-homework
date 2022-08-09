85bb0c275682630fd759d47cda9ab6c9a6bde4d8
ChangAn <1533167307@qq.com>
2022-08-09 21:21:22 +0800

    0-0

--- 第九章编程题/第九章编程.c
+++ 第九章编程题/第九章编程.c
@@ -2,12 +2,20 @@
 #include <stdio.h>
 void two(void);//2
 void chline(char ch, int i, int j);//2
+void four(void);//4
+double average(double i, double j);//4
+void six(void);//6
+void compare(double* ptr1, double* ptr2, double* ptr3);//6
 int main(void)
 {
-	two();
+	//two();
+	//four();
+	six();
 	return 0;
 }
 
+
+//2****************************************************************************************
 void two(void)
 {
 	char ch;
@@ -29,4 +37,50 @@ void chline(char ch, int i, int j)
 			putchar(ch);
 		putchar('\n');
 	}
+}
+
+//4*********************************************************************************************
+void four(void)
+{
+	double i, j;
+	printf("输入1\n");
+	scanf("%lf", &i);
+	printf("输入2\n");
+	scanf("%lf", &j);
+	printf("%lf",average(i,j));
+}
+
+double average(double i, double j)
+{
+	double a = 1 / i;
+	double b = 1 / j;
+	return 1 / ((a + b) / 2);
+}
+
+//6***********************************************************************************************
+void six(void)
+{
+	double i, j, k;
+	double* ptr1, * ptr2, * ptr3;
+	ptr1 = &i; ptr2 = &j; ptr3 = &k;
+	printf("输入1\n"); scanf("%lf", &i);
+	printf("输入2\n"); scanf("%lf", &j);
+	printf("输入3\n"); scanf("%lf", &k);
+	compare(ptr1, ptr2, ptr3);
+	printf("%lf %lf %lf",i,j,k);
+	return;
+}
+
+void compare(double* ptr1, double *ptr2, double *ptr3)
+{
+	double num[3] = { *ptr1 ,*ptr2 ,*ptr3 };
+	for (int i = 0; i < 2; i++)
+		for (int j = i + 1; j < 3; j++)
+			if(num[i]>num[j])
+			{
+				double a = num[j];
+				num[j] = num[i];
+				num[i] = a;
+		    }
+	*ptr1 = num[0]; *ptr2 = num[1]; *ptr3 = num[2];
 }
