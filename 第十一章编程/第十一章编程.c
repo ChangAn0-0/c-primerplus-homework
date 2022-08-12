d9bacb84b9c1afe5ed02851ea2277d4ca9c41eab
ChangAn <1533167307@qq.com>
2022-08-12 22:20:13 +0800

    0-0

--- 第十一章编程/第十一章编程.c
+++ 第十一章编程/第十一章编程.c
@@ -1,13 +1,20 @@
+#define  _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <ctype.h>
 void one(void);//1
 void in(char* ptr);//1
 void three(void);//3
 int num(char letter[]);//3
+void five(void);//5
+char* compare(char str1[], char ch, int a);//5
+void eight(void);//8
+char* string_in(char str1[], char str2[], int a, int b);//8
 int main(void)
 {
 	//one();
-	three();
+	//three();
+	//five();
+	eight();
 	return 0;
 }
 
@@ -45,8 +52,72 @@ int num(char letter[])
 	do {
 		letter[i]=ch;
 		i++;
-	} while (!isblank(ch = getchar())&&!(ch=='\n'));
+	} while ((!isblank(ch = getchar())) && !(ch == '\n'));
 	while ((ch = getchar()) != EOF)
 		continue;
 	return i;
+}
+
+//5**************************************************************************************
+void five(void)
+{
+	char ch;
+	char* ptr;
+	int i = 0;
+	char str1[40];
+	printf("ÊäÈë×Ö·û´®1");
+	while ((str1[i] = getchar()) != '\n')
+		i++;
+	printf("ÊäÈë²éÕÒ×Ö·û");
+	scanf("%c", &ch);
+	ptr = compare(str1, ch, i);
+	printf("%c %p", *ptr, ptr);
+}
+
+char* compare(char str1[], char ch, int a)
+{
+	for (int i = 0; i < a; i++)
+	{
+		if (str1[i] == ch)
+			return str1 + i;
+	}
+	return 0;
+}
+
+//8**************************************************************************************
+void eight(void)
+{
+	char ch;
+	char* ptr;
+	int i = 0; int n = 0;
+	char str1[40];
+	char str2[40];
+
+	printf("ÊäÈë×Ö·û´®1");
+	while ((str1[i] = getchar()) != '\n')
+		i++;
+	printf("ÊäÈë×Ö·û´®2");
+	while ((str2[i] = getchar()) != '\n')
+		n++;
+	ptr = string_in(str1, str2, i, n);
+	printf("%c %p", *ptr, ptr);
+}
+
+char* string_in(char str1[], char str2[],int a, int b)
+{
+	_Bool num = 0;
+	for (int i = 0; i < b; i++) 
+	{
+		for (int n = 0; n < a; n++)
+		{
+			if (str1[n] == str2[i])
+				num = 1;
+		}
+		if (!num)
+			return 0;
+		num = 0;
+	}
+	for (int i = 0; i < a; i++)
+		if (str1[i] == str2[0])
+			return str1 + i;
 }
