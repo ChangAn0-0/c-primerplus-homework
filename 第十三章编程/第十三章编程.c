834b9052747b5a00bba1fa1c597fc196776c432d
ChangAn <1533167307@qq.com>
2022-08-19 17:44:25 +0800

    0-0

--- 第十三章编程/第十三章编程.c
+++ 第十三章编程/第十三章编程.c
@@ -2,13 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
 char* s_gets(char* st, int n);//1
 void one(void);//1
 void two(char* file1, char* file2);//
+void five_review(char* ch, char* file);//five_review
+void three(void);//3
+
 int main(int argc,char* argv[])
 {
 	//one();
-	two(argv[1], argv[2]);
+	//two(argv[1], argv[2]);
+	//five_review(argv[1], argv[2]);
+	three();
 	return 0;
 }
 
@@ -65,7 +72,7 @@ void two(char *file1,char* file2)
 		fputs("file2ÉêÇë»º³åÊ§°Ü", stderr);
 		exit(EXIT_FAILURE);
 	}
-	while ((bytes = fread(temp, sizeof(char), 4096, ptr1)) > 0)
+	while((bytes = fread(temp, sizeof(char), 4096, ptr1)) > 0)
 		fwrite(temp, sizeof(char), bytes, ptr2);
 	if(ferror(ptr1)!=0)
 		fputs("file¶ÁÈ¡Ê§°Ü", stderr);
@@ -73,6 +80,55 @@ void two(char *file1,char* file2)
 		fputs("fileÊäÈëÊ§°Ü", stderr);
 	fclose(ptr1);
 	fclose(ptr2);
+}
+
+//¸´Ï°Ìâ5*************************************************************************************
+void five_review(char *ch,char* file)
+{
+	char line[256];
+	FILE* ptr1;
+	if ((ptr1 = fopen(file, "r")) == NULL)
+	{
+		fprintf(stderr, "failed to open,\"%s\"", file);
+		exit(EXIT_FAILURE);
+	}
+	
+	while ((fgets(line, 256, ptr1)) != NULL)
+	{
+		if (strchr(line, *ch) != NULL)
+			puts(line);
+	}
+}
+
+//3*************************************************************************
+void three(void)
+
+{
+	size_t bytes;
+	FILE* ptr; 
+	char name[40];
+	char copy[256];
+	printf("ÊäÈëÎÄ¼þÃû");
+	//s_gets(name, 40);
+	//fgets(name, 40, stdin);
+	scanf("%s", name);
+	if ((ptr = fopen(name, "r+")) == NULL)
+	{
+		fprintf(stderr, "failed to open,%s", name);
+		exit(EXIT_FAILURE);
+	}
+	setvbuf(ptr, NULL, _IOFBF, 256);
+	while ((bytes = fread(copy,1, 256, ptr)) > 0)
+	{
+		char* s = copy;
+		while (*s)
+		{
+			*s = toupper(*s);
+			s++;
+		}
+		rewind(ptr);
+		fwrite(copy, 1, bytes, ptr);
+	}
 }
 
 
