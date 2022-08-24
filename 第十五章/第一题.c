984583b658403221fe2b9fa639b607991343cfc3
ChangAn <1533167307@qq.com>
2022-08-24 08:37:58 +0800

    0-0

--- 第十五章/第一题.c
+++ 第十五章/第一题.c
@@ -1,7 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int main(void)
 {
+	int sum=0;
+	int i = 0;
+	char ch;
 	char num[8] = { 0 };
-	scanf("%s", num);
+	while ((ch = getchar()) != '\n')
+	{
+		num[i] = ch-'0';
+		i++;
+	}
+	for (int i = 0; i < 8; i++)
+	{
+		sum += num[i] ;
+		if (i);
+		sum *= 2;
+	}
+	printf("%d", sum/2);
 	return 0;
 }
